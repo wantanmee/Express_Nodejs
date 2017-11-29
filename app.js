49387224d2301d91338907ae3e26025d26f20e36
Kate Chen <kate.jy.chen@gmail.com>
2017-11-29 17:33:18 +0800

    add controller and routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
